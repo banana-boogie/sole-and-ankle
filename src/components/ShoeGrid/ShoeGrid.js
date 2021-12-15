e21c2a9232c96cbe34f8ffa017e7f210b2bfe859
Banana Man <banana@bananaboogie.com>
2021-12-14 16:48:36 -0800

    Shoe grid complete!

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -8,12 +8,29 @@ const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 36px;
+  flex-wrap: wrap;
+`;
+
+/**
+ * This wrapper is used to store the flex related styles
+ * for each Shoe from ShoeCard.
+ * This way is preferred because it enables the ShoeCard component to be
+ * re-used in a different context (i.e. other than flexbox)
+ */
+const ShoeWrapper = styled.div`
+  max-width: 340px;
+  flex: 1 0 340px;
+`;
 
 export default ShoeGrid;
